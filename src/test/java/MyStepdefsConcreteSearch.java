229db15c8f062b8c4ce668900146402217e9189b
Sören Zacharias <soeren.zacharias@student.fh-kiel.de>
2023-06-10 13:25:33 +0200

    steps added

---
+++ src/test/java/MyStepdefsConcreteSearch.java
@@ -0,0 +1,21 @@
+import io.cucumber.java.de.Angenommen;
+import io.cucumber.java.de.Dann;
+import io.cucumber.java.de.Wenn;
+
+public class MyStepdefsConcreteSearch {
+
+    @Angenommen("folgende Bücher existieren")
+    public void folgendeBucherExistieren() {
+
+    }
+
+    @Wenn("Bücher zur Suche hinzugefügt werden")
+    public void bucherZurSucheHinzugefugtWerden() {
+
+    }
+
+    @Dann("sollten die Bücher für die Suche verfügbar sein")
+    public void solltenDieBucherFurDieSucheVerfugbarSein() {
+
+    }
+}
